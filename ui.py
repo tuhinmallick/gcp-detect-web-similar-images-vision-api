c555fe4af4029b7691d608b45b5283eb17af7760
Tuhin Mallick <tuhin.mllk@gmail.com>
2024-02-04 03:12:21 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -8,4 +8,4 @@ gr.Interface(fn=detect_web,
                                              debug=False, 
                                              max_threads=8,
                                              server_name='0.0.0.0', 
-                                             server_port=80)
+                                             server_port=8080)
