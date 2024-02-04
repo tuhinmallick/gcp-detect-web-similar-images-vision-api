34dbc7903d72ad4cc5d3328621cff71b23b6bbf4
Tuhin Mallick <tuhin.mllk@gmail.com>
2024-02-04 03:00:37 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -3,10 +3,9 @@ from main import detect_web
 
 gr.Interface(fn=detect_web, 
              inputs=gr.Image(type="filepath"),
-             outputs=[gr.Gallery().style(grid=4),
-             gr.Textbox()],
+             outputs=[gr.Gallery(), gr.Textbox()],
              examples=["image.webp"]).launch(share=False, 
-                                                debug=False, 
-                                                max_threads=8,
-                                                server_name='0.0.0.0', 
-                                                server_port=80)
+                                             debug=False, 
+                                             max_threads=8,
+                                             server_name='0.0.0.0', 
+                                             server_port=80)
